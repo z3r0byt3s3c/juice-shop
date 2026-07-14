78a525c331470086622da54cd781de19f979a925
DEBUG MODE <kevinw3b@w3b.codes>
2026-07-14 08:29:20 +0300

    Fix SQL injection in login route with bind parameters

--- routes/login.ts
+++ routes/login.ts
@@ -31,7 +31,10 @@ export function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+    models.sequelize.query(
+      'SELECT * FROM Users WHERE email = $1 AND password = $2 AND deletedAt IS NULL',
+      { model: UserModel, plain: true, bind: [req.body.email || '', security.hash(req.body.password || '')] }
+    )
       .then((authenticatedUser) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
